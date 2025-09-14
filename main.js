aa17976244bb9c06cda47cc8dd6ea3e3ece09ba0
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-09-14 11:11:34 -0500

    moved round counter, score variables, getComputerChoice, getHumanChoice inside playGame() so they can be called multiple times

--- main.js
+++ main.js
@@ -1,51 +1,52 @@
-// RPS logic
-
-function getComputerChoice() {
-    let computerSelection = Math.floor(Math.random() * (3 - 1 + 1)) + 1
-    if (computerSelection === 1) {
-        computerSelection = "rock"
-    } else if (computerSelection === 2) {
-        computerSelection = "paper"
-    } else {
-        computerSelection = "scissors"
-    }
-    return computerSelection;
-}
-
-function getHumanChoice() {
-    let humanSelection = prompt("rock, paper or scissors?")
-    if (humanSelection === "rock" || humanSelection === "Rock" || humanSelection === "ROCK") {
-        humanSelection = "rock"
-        return humanSelection;
-    } else if (humanSelection === "paper" || humanSelection === "Paper" || humanSelection === "PAPER") {
-        humanSelection = "paper"
-        return humanSelection;
-    } else if (humanSelection == "scissors" || humanSelection ==="Scissors" || humanSelection === "SCISSORS") {
-        humanSelection = "scissors"
-        return humanSelection;
-    } else {
-        alert("Please select rock, paper or scissors only")
-        return getHumanChoice();
-    }
-    
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-let roundCounter = 0;
-
-// Score variables
-
-let humanScore = 0;
-let computerScore = 0;
 
 function playGame() {
+
+    let roundCounter = 0;
+
+    // Score variables
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // RPS logic
+
+    function getComputerChoice() {
+        let computerSelection = Math.floor(Math.random() * (3 - 1 + 1)) + 1
+        if (computerSelection === 1) {
+            computerSelection = "rock"
+        } else if (computerSelection === 2) {
+            computerSelection = "paper"
+        } else {
+            computerSelection = "scissors"
+        }
+        return computerSelection;
+    }
+
+    function getHumanChoice() {
+        let humanSelection = prompt("rock, paper or scissors?")
+        if (humanSelection === "rock" || humanSelection === "Rock" || humanSelection === "ROCK") {
+            humanSelection = "rock"
+            return humanSelection;
+        } else if (humanSelection === "paper" || humanSelection === "Paper" || humanSelection === "PAPER") {
+            humanSelection = "paper"
+            return humanSelection;
+        } else if (humanSelection == "scissors" || humanSelection === "Scissors" || humanSelection === "SCISSORS") {
+            humanSelection = "scissors"
+            return humanSelection;
+        } else {
+            alert("Please select rock, paper or scissors only")
+            return getHumanChoice();
+        }
+
+    }
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
     function playRound(humanChoice, computerChoice) {
 
         console.log(humanChoice);
         console.log(computerChoice);
+
 
         if (humanChoice == "rock" && computerChoice == "scissors") {
             console.log("You win! Rock beats scissors!")
@@ -83,7 +84,7 @@ function playGame() {
         console.log(humanScore);
         console.log(computerScore);
 
-        if (roundCounter == 5) {
+        if (roundCounter === 5) {
             if (humanScore > computerScore) {
                 console.log("Congradulations! You win!")
             } else {
